29b25002fe1493ed5def20e8d2057008132c42e8
werewolf lord <40964777+alwx2008@users.noreply.github.com>
2024-05-13 04:13:34 +0000

    Create Classes

---
+++ src/classes/Field.py
@@ -0,0 +1,22 @@
+class Field():
+
+    WIDTH = 10
+    HEIGHT = 22
+
+    def __init__(self, state=None):
+        pass
+
+    def copy(self):
+        """
+        Returns a shallow copy of the field.
+        """
+        pass
+
+    def drop(self, tetromino, column):
+        pass
+
+    def count_gaps(self):
+        pass
+
+    def height(self):
+        pass
---
+++ src/classes/Tetromino.py
@@ -0,0 +1,58 @@
+class Tetromino():
+    def __init__(self, state):
+        pass
+
+    @staticmethod
+    def ITetromino():
+        return Tetromino(
+            [
+                ['I', 'I', 'I', 'I']
+            ]
+        )
+    
+    @staticmethod
+    def OTetromino():
+        return Tetromino(
+            [
+                ['O','O'],
+                ['O', 'O']
+            ]
+        )
+    
+    @staticmethod
+    def TTetromino():
+        pass
+
+    @staticmethod
+    def STetromino():
+        pass
+    
+    @staticmethod
+    def ZTetromino():
+        pass
+
+    @staticmethod
+    def JTetromino():
+        pass
+
+    @staticmethod
+    def LTetromino():
+        pass
+
+    def width(self):
+        pass
+
+    def height(self):
+        pass
+
+    def rotate(self, change):
+        pass
+
+    def rotate_left(self):
+        pass
+
+    def rotate_right(self):
+        pass
+
+    def flip(self):
+        pass
